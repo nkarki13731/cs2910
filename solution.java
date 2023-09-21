59b06f5591a22b02ad8bd025b6501089ded59aa4
Nima Sherpa <chhetunima202107@gmail.com>
2023-09-21 00:06:01 -0300

    Create main

---
+++ solution.java
@@ -0,0 +1,12 @@
+public class solution {
+
+    public static void main(String[] args)  {
+        System.out.println("Machikne");
+    }
+
+    public static class blockhouse {
+        public static void main(String args[]){
+            System.out.println("HEllo");
+        }
+    }
+}
